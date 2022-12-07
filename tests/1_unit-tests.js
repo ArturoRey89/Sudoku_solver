0245c3ae261105f408136ab9f60254ecfcb56a79
ArturoRey89 <arturoreyes89@gmail.com>
2022-12-07 03:09:59 +0100

    feat: all basic tests added

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,29 +2,46 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
+const Puzzles = require("../controllers/puzzle-strings.js").puzzlesAndSolutions;
+
 const solver = new Solver();
 const validate = solver.validate;
 const checkRow = solver.checkRowPlacement;
 const checkCol = solver.checkColPlacement;
 const checkReg = solver.checkRegionPlacement;
 const solve = solver.solve;
-
+console.log(validate)
 suite("Unit Tests", () => {
     suite("Solver.validate()", () => {
       test("handles a valid puzzle string of 81 characters", (done) => {
         assert.isTrue(validate(validPuzzle));
         done();
       })
+
       test("handles a puzzle string with invalid characters (not 1-9 or .)", (done) => {
-        assert.equal(invalidPuzzle1, { error: "Invalid characters in puzzle" });
-        assert.equal(invalidPuzzle2, { error: "Invalid characters in puzzle" });
-        assert.equal(invalidPuzzle3, { error: "Invalid characters in puzzle" });
+        assert.equal(
+          invalidPuzzle1,
+          { error: "Invalid characters in puzzle" },
+          "should catch invalid 'g' in input"
+        );
+        assert.equal(
+          invalidPuzzle2,
+          { error: "Invalid characters in puzzle" },
+          "should catch invalid '\\' in input"
+        );
+        assert.equal(
+          invalidPuzzle3,
+          { error: "Invalid characters in puzzle" },
+          "should catch invalid '?' in input"
+        );
         done();
       })
+
       test("handles a puzzle string that is not 81 characters in length", (done) => {
-        assert.equal(invalidPuzzle4, {
-          error: "Expected puzzle to be 81 characters long",
-        });
+        assert.equal(
+          invalidPuzzle4.error,
+          "Expected puzzle to be 81 characters long"
+        );
         done();
       })
     });
@@ -32,11 +49,13 @@ suite("Unit Tests", () => {
 
     suite("Solver.checkRowPlacement()", () => {
       test("handles a valid row placement", (done) => {
-        assert.fail(checkRow(validPuzzle, 2, 2, 2));
+        assert.isTrue(checkRow(validPuzzle1, 3, 1, 7).vailid);
         done();
       })
       test("handles an invalid row placement", (done) => {
-        assert.fail()
+        let rowError = checkRow(validPuzzle1, 2, 1, 7);
+        assert.isFalse(rowError.valid);
+        assert.equal(rowError.conflict[0], "row");
         done();
       })
     });
@@ -44,11 +63,13 @@ suite("Unit Tests", () => {
 
     suite("Solver.checkColPlacement()", () => {
       test("handles a valid column placement", (done) => {
-        assert.fail()
+        assert.isTrue(checkCol(validPuzzle1, 3, 1, 7).valid);
         done();
       })
       test("handles an invalid column placement", (done) => {
-        assert.fail()
+        let colError = checkRow(validPuzzle1, 3, 1, 8);
+        assert.isFalse(colError.valid);
+        assert.equal(colError.conflict[0], "column");
         done();
       })
     });
@@ -56,11 +77,13 @@ suite("Unit Tests", () => {
 
     suite("Solver.checkRegionPlacement()", () => {
       test("handles a valid region (3x3 grid) placement", (done) => {
-        assert.fail()
+        assert.isTrue(checkReg(validPuzzle1, 3, 1, 7).valid);
         done();
       })
       test("handles an invalid region (3x3 grid) placement", (done) => {
-        assert.fail()
+        let regError = checkRow(validPuzzle1, 3, 1, 6)
+        assert.isFalse(regError.valid);
+        assert.equal(regError.conflict[0], "region");
         done();
       })
     });
@@ -68,26 +91,38 @@ suite("Unit Tests", () => {
 
     suite("Solver.solve()", () => {
       test("Valid puzzle strings pass the solver", (done) => {
-        assert.fail()
+        assert.isEmpty(solve(validPuzzle1).error);
+        assert.isEmpty(solve(validPuzzle2).error);
+        assert.isEmpty(solve(validPuzzle3).error);
+        assert.isEmpty(solve(validPuzzle4).error);
+        assert.isEmpty(solve(validPuzzle5).error);
         done();
       })
       test("Invalid puzzle strings fail the solver", (done) => {
-        assert.fail()
+        assert.equal(solve(noSolutionPuzzle).error, "Puzzle cannot be solved");
         done();
       })
       test("Solver returns the expected solution for an incomplete puzzle", (done) => {
-        assert.fail()
+        assert.equal(solve(validPuzzle1), Puzzles[0][1]);
+        assert.equal(solve(validPuzzle2), Puzzles[1][1]);
+        assert.equal(solve(validPuzzle3), Puzzles[2][1]);
+        assert.equal(solve(validPuzzle4), Puzzles[3][1]);
+        assert.equal(solve(validPuzzle5), Puzzles[4][1]);
         done();
       })
     })
 });
 
-const validPuzzle =
-  "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+const validPuzzle1 = Puzzles[0][0];
+const validPuzzle2 = Puzzles[1][0];
+const validPuzzle3 = Puzzles[2][0];
+const validPuzzle4 = Puzzles[3][0];
+const validPuzzle5 = Puzzles[4][0];
+
 const invalidPuzzle1 =
   "1.5..2.g4..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
 const invalidPuzzle2 =
-  "1.5..2.g4..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+  "1.5..2.\\4..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
 const invalidPuzzle3 =
   "1.5..2.?4..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
 const invalidPuzzle4 =
